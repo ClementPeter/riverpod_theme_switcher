44eaaaf4c1f38b3baa8d0a4c6fc86cdb24b44726
ClementPeter <clementpeter2000@gmail.com>
2023-07-23 18:48:21 +0100

    “theme_switcher_provider”

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:theme_switcher_riverpod/app_theme.dart';
-// import 'package:theme_switcher_riverpod/theme_provider.dart';
-
-// void main() {
-//   runApp(
-//     const ProviderScope(
-//       child: MyApp(),
-//     ),
-//   );
-// }
-
-// //App Theming with Change Notifier Provider
-// class MyApp extends ConsumerWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final appThemeMode = ref.watch(appThemeChangeNotifierProvider);
-//     return MaterialApp(
-//       title: 'Riverpod theme Demo',
-//       //specifies the current theme of the App
-//       theme: AppTheme.lightTheme,
-//       //setting 'darkTheme' helps enable system dark mode
-//       darkTheme: AppTheme.darkTheme,
-//       themeMode:
-//           appThemeMode.isDarkModeEnabled ? ThemeMode.dark : ThemeMode.light,
-//       home: const MyHomePage(title: 'Riverpod theme Demo'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//         centerTitle: true,
-//       ),
-//       body: const Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: <Widget>[
-//           Expanded(
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text('Light Mode'),
-//                 DarkModeSwitch(),
-//                 Text('Dark Mode'),
-//               ],
-//             ),
-//           ),
-//           //
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class DarkModeSwitch extends ConsumerWidget {
-//   const DarkModeSwitch({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final switchThemeMode = ref.watch(appThemeChangeNotifierProvider);
-//     return Switch(
-//       //value:ref.watch(appThemeChangeNotifierProvider.notifier).isDarkModeEnabled,
-//       value: switchThemeMode.isDarkModeEnabled,
-//       onChanged: (bool enabled) {
-//         if (enabled) {
-//           switchThemeMode.setDarkTheme();
-//         } else {
-//           switchThemeMode.setLightTheme();
-//         }
-//       },
-//     );
-//   }
-// }
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-//App Theming with State Notifier Provider
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:theme_switcher_riverpod/app_theme.dart';
-//import 'package:theme_switcher_riverpod/app_theme.dart';
 import 'package:theme_switcher_riverpod/theme_provider.dart';
 
 void main() {
@@ -121,23 +11,21 @@ void main() {
   );
 }
 
+//App Theming with Change Notifier Provider
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    //
-    //final appThemeMode = ref.watch(appThemeStateNotifierProvider.notifier);
-
+    final appThemeMode = ref.watch(appThemeChangeNotifierProvider);
     return MaterialApp(
       title: 'Riverpod theme Demo',
-      //theme: appThemeMode,
-      theme: ref.watch(appThemeStateNotifierProvider),
+      //specifies the current theme of the App
+      theme: AppTheme.lightTheme,
       //setting 'darkTheme' helps enable system dark mode
-      //darkTheme: AppTheme.darkTheme,
-      // themeMode: appThemeMode.mode == ThemeMode.dark
-      //     ? ThemeMode.dark
-      //     : ThemeMode.light,
+      darkTheme: AppTheme.darkTheme,
+      themeMode:
+          appThemeMode.isDarkModeEnabled ? ThemeMode.dark : ThemeMode.light,
       home: const MyHomePage(title: 'Riverpod theme Demo'),
     );
   }
@@ -160,34 +48,20 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: Column(
+      body: const Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Light Mode'),
-              // Consumer(builder: (context, ref, child) {
-              //   return DarkModeSwitch();
-              // }),
-              DarkModeSwitch(),
-              Text('Dark Mode'),
-            ],
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Light Mode'),
+                DarkModeSwitch(),
+                Text('Dark Mode'),
+              ],
+            ),
           ),
-          Center(
-            child: Consumer(builder: (context, ref, child) {
-              return ElevatedButton(
-                onPressed: () {
-                  final themeMode =
-                      ref.watch(appThemeStateNotifierProvider.notifier);
-                  themeMode.toggleTheme();
-                },
-                child: const Text('Toggle Team'),
-              );
-            }
-                //child:
-                ),
-          ),
+          //
         ],
       ),
     );
@@ -197,22 +71,19 @@ class _MyHomePageState extends State<MyHomePage> {
 class DarkModeSwitch extends ConsumerWidget {
   const DarkModeSwitch({super.key});
 
-  void toggleTheme(WidgetRef ref) {
-    ref.read(appThemeStateNotifierProvider.notifier).toggleTheme();
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Switch.adaptive(
-        //value: ref.watch(appThemeStateNotifierProvider.notifier)._mode == ThemeMode.dark,
-
-        // value: ref.watch(appThemeStateNotifierProvider.notifier).mode ==
-        //     ThemeMode.dark ? ,
-        value: ref.watch(appThemeStateNotifierProvider.notifier).mode ==
-            ThemeMode.light,
-        onChanged: (value) => toggleTheme(ref)
-        // ref.read(appThemeStateNotifierProvider.notifier).toggleTheme(),
-
-        );
+    final switchThemeMode = ref.watch(appThemeChangeNotifierProvider);
+    return Switch(
+      //value:ref.watch(appThemeChangeNotifierProvider.notifier).isDarkModeEnabled,
+      value: switchThemeMode.isDarkModeEnabled,
+      onChanged: (bool enabled) {
+        if (enabled) {
+          switchThemeMode.setDarkTheme();
+        } else {
+          switchThemeMode.setLightTheme();
+        }
+      },
+    );
   }
 }
--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -3,143 +3,47 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:theme_switcher_riverpod/app_theme.dart';
 
-// ///Declare ChangeNotifierProvider for AppThemeNotifier
-// final appThemeChangeNotifierProvider =
-//     ChangeNotifierProvider<AppThemeChangeNotifier>((ref) {
-//   return AppThemeChangeNotifier();
-// });
-
-// //provider class  to toggle App theme
-// class AppThemeChangeNotifier extends ChangeNotifier {
-//   //toggle theme modes
-//   bool isDarkModeEnabled = false;
-//   //SharedPreferences _prefs;
-
-// //constructor - called first when class is build
-//   AppThemeChangeNotifier() {
-//     loadTheme();
-//   }
-
-//   //enable light mode
-//   void setLightTheme() {
-//     isDarkModeEnabled = false;
-//     saveTheme();
-//     notifyListeners();
-//   }
-
-//   //enable dark mode
-//   void setDarkTheme() {
-//     isDarkModeEnabled = true;
-//     saveTheme();
-//     notifyListeners();
-//   }
-
-//   //method to save current theme to SharedPreference
-//   Future<void> saveTheme() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.setBool('isDarkModeEnabled', isDarkModeEnabled);
-//   }
-
-//   //Method to load the saved theme from SharedPreference
-//   Future<void> loadTheme() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.getInt('isDarkModeEnabled') ?? false;
-//     notifyListeners();
-//   }
-// }
-//
-//
-//
-//
-//
-//
-//
-
-///Theming with ChangeNotifierProvider and shared preference
-// ///Theming with ChangeNotifierProvider and for AppThemeNotifier
-// final appThemeChangeNotifierProvider =
-//     ChangeNotifierProvider<AppThemeChangeNotifier>((ref) {
-//   return AppThemeChangeNotifier();
-// });
-
-// //provider class  to toggle App theme
-// class AppThemeChangeNotifier extends ChangeNotifier {
-//   //toggle theme modes
-//   bool isDarkModeEnabled = false;
-
-//   //enable light mode
-//   void setLightTheme() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.setString('theme', 'light')
-//     isDarkModeEnabled = false;
-//     notifyListeners();
-//   }
-
-//   //enable dark mode
-//   void setDarkTheme() {
-//     isDarkModeEnabled = true;
-//     notifyListeners();
-//   }
-
-//   //void  toggleTheme
-// }
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-///Re-writing Theming using STATE NOTIFIER PROVIDER
-final appThemeStateNotifierProvider =
-    StateNotifierProvider<AppThemeStateNotifier, ThemeData>((ref) {
-  return AppThemeStateNotifier();
+///Declare ChangeNotifierProvider for AppThemeNotifier
+final appThemeChangeNotifierProvider =
+    ChangeNotifierProvider<AppThemeChangeNotifier>((ref) {
+  return AppThemeChangeNotifier();
 });
 
-class AppThemeStateNotifier extends StateNotifier<ThemeData> {
-  //
-  ThemeMode _mode;
+//provider class  to toggle App theme
+class AppThemeChangeNotifier extends ChangeNotifier {
+  //toggle theme modes
+  bool isDarkModeEnabled = false;
+  //SharedPreferences _prefs;
 
-  AppThemeStateNotifier({ThemeMode mode = ThemeMode.dark})
-      : _mode = mode,
-        super(AppTheme.darkTheme) {
-    print('::app theme default constructor -> DARK::');
-    getTheme();
+//constructor - called first when class is build
+  AppThemeChangeNotifier() {
+    loadTheme();
   }
 
-  ThemeMode get mode => _mode;
-
-  //Get the Starting App THEME
-  void getTheme() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final theme = prefs.getString('theme');
-
-    if (theme == 'light') {
-      _mode = ThemeMode.light;
-      state = AppTheme.lightTheme;
-      //prefs.setString('theme', 'light');
-    } else {
-      _mode = ThemeMode.dark;
-      state = AppTheme.darkTheme;
-      //prefs.setString('key', 'dark');
-    }
+  //enable light mode
+  void setLightTheme() {
+    isDarkModeEnabled = false;
+    saveTheme();
+    notifyListeners();
   }
 
-  void toggleTheme() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+  //enable dark mode
+  void setDarkTheme() {
+    isDarkModeEnabled = true;
+    saveTheme();
+    notifyListeners();
+  }
 
-    if (_mode == ThemeMode.dark) {
-      _mode = ThemeMode.light;
-      state = AppTheme.lightTheme;
-      prefs.setString('theme', 'light');
-    } else {
-      _mode = ThemeMode.dark;
-      state = AppTheme.darkTheme;
-      prefs.setString('theme', 'dark');
-    }
+  //method to save current theme to SharedPreference
+  Future<void> saveTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkModeEnabled', isDarkModeEnabled);
+  }
+
+  //Method to load the saved theme from SharedPreference
+  Future<void> loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getInt('isDarkModeEnabled') ?? false;
+    notifyListeners();
   }
 }
